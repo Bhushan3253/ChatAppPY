e5e0ef095a49473a0cf83c606bd12e4e2c73a334
Bhushan <bhushannagpure25@gmail.com>
2026-06-17 22:19:39 +0530

    i

--- django-backend/core/asgi.py
+++ django-backend/core/asgi.py
@@ -3,7 +3,6 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from core.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
@@ -11,6 +10,7 @@ django.setup()
 django_asgi_app = get_asgi_application()
 
 import chat.routing
+from core.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
